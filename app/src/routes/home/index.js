2f828dd9cfa79aa0be876fd66f5ef15b97a5429d
SON <wjdgh2777@naver.com>
2022-05-17 01:18:22 +0900

    front JS

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,6 +6,6 @@ const router = express.Router();
 const ctrl = require("./home.ctrl")
 
 router.get("/", ctrl.hello);
-router.get("/login", ctrl.login);
+router.get("/login", ctrl.login);  
 
 module.exports = router;
