6579104dce4fae93ab55c651481eae174193a332
SON <wjdgh2777@naver.com>
2022-05-19 00:35:10 +0900

    userStorage 모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,27 @@
+//유저 정보를 가지고있음
+"use strict";
+
+class UserStorage{
+    static #users = {//static선언하면 클래스자체에서 변수접근가능
+        //#는 은닉화임(public -> private)
+        id: ["SoNB", "SoNB1", "SoNB2"],
+        psword: ["123", "1234", "12345"],
+        name: ["손정호","정호","호"],
+    };
+
+    static getUsers(...fields){//은닉화된 변수를 가져다쓰기위한 함수
+        //매개변수로 몇개를 받아올지 모름(받아온것들만 출력)
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            //fields의 요소들을 reduce로 순회시켜 newUsers로 생성시킴
+            //newUsers에는 fields라는 배열의 초기값이 들어감
+            if(users.hasOwnProperty(field)){//users에 해당키값이 있나?
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});//newUsers의 초기값을 내맘대로 {}빈 오브젝트로 생성
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const users = {
-    id: ["SoNB", "SoNB1", "SoNB2"],
-    psword: ["123", "1234", "12345"],
-};
+const UserStorage = require("../../models/UserStorage");
 
 const output = {
     hello : (req, res) => {
@@ -19,19 +16,20 @@ const process = {
         const id = req.body.id,
         psword = req.body.psword;
 
+        const users = UserStorage.getUsers("id", "psword");
+        //id와 psword필드만 가져옴
+        const response = {};//오브젝트 변수를 만듬
         if(users.id.includes(id)){//front에서 전달한 id가 서버의 id에 있다면
             const idx = users.id.indexOf(id);
             if(users.psword[idx] === psword){
-                return res.json({
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
 
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다."
+        return res.json(response);
     },
 };
 
