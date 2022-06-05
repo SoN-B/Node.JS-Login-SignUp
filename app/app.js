50e564d45b5596df2b6cb53218fb6b5f18ca7a1d
SON <wjdgh2777@naver.com>
2022-06-05 16:27:41 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -7,27 +7,26 @@ const dotenv = require("dotenv");
 /*환경변수 관리
 외부에 노출되어선 안되는것들을 환경변수로 감춤
 어떤 OS든간에, 모두 동일하게 환경변수 사용가능*/
-const morgan = require('morgan');
 const app = express();
-dotenv.config();//환경변수 모듈 동작
+dotenv.config(); //환경변수 모듈 동작
 //.env에 등록되어있는 변수들을 Node.js에서 접근할 수 있도록 process.env에 등록시켜줌
 
 //라우팅
 const home = require("./src/routes/home");
-const accessLogStream = require("./src/config/log");
 
 //웹세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 /*bodyParser : URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될경우
 제대로 인식되지 않는 문제 해결*/
-app.use(morgan("dev"));//콘솔도 같이 출력
-app.use(morgan("common", { stream : accessLogStream }));
-//log를 파일로 저장(common형식으로), stream으로 통신
 
-app.use("/",home); //use -> 미들 웨어를 등록해주는 메서드.
+app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드.
 
 module.exports = app;
+//* : 일반 정보
+//! : 중요한 주의사항
+//? : 궁금한 사항
+//TODO : 추가적으로 해야 할 일
---
+++ app/src/config/logger.js
@@ -0,0 +1,61 @@
+const { createLogger, transports, format } = require("winston");
+const { combine, timestamp, simple, colorize, printf, label } = format;
+
+const printFormat = printf(({timestamp, label, level, message}) => {//사용자지정포맷
+    return `${timestamp} [${label}] ${level} : ${message}`//app.js의 message
+}); //마지막이 최종 출력포맷이됨
+
+const printLogFormat = {
+    file: combine(//추가해주고 싶은 포맷들을 결합
+    //colorize(), //console에 찍히는 에러에 색상입힘
+    //winston.format.simple(), //간단한 형식으로
+    label({
+        label: "백엔드 맛보기"//어떤프로젝트의 로그인지 알려줌
+    }),
+
+    timestamp({
+        format: "YYYY-MM-DD HH:mm:dd"
+    }),
+    printFormat
+    ),
+
+    console: combine(
+        colorize(),
+        simple(),
+    )
+};
+
+const opts = {
+    file: new transports.File({//log를 파일에저장
+        filename: "access.log",
+        dirname: "./logs",
+        level: "info",//2레벨인데, 다른곳의 2,1,0레벨인 로그들 출력가능
+        format: printLogFormat.file, //format이 file에 저장되기위한 형태
+    }),
+    console: new transports.Console({//log를 콘솔로찍음
+        level: "info",
+        format: printLogFormat.console, //format이 console에 찍히기위한 형태
+    }),
+}
+
+const logger = createLogger({
+    transports: [opts.file],
+});
+
+if (process.env.NODE_ENV !== "production") { //실제 서비스중인 서버가 아니면
+    logger.add(opts.console);
+} //실제 서비스중인 서버와 개발중인 서버를 구분지을 수 있음
+
+module.exports = logger;
+
+/* level
+const levels = {
+  error: 0, -> 제일 높은거
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6 -> 제일 낮은거
+};
+*/
